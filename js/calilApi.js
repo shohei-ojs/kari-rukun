90976590d0fa9df99fec0bb9042066d1abb5cbac
shohei-ojs <shohei.ojs@gmail.com>
2018-03-17 17:49:03 +0900

    ポーリング処理導入せねばづら

--- js/calilApi.js
+++ js/calilApi.js
@@ -3,10 +3,27 @@ export function getLibrary (coords) {
   return $.ajax({
     type: "GET",
     dataType: "jsonp",
-    url: "http://api.calil.jp/library?appkey=e669768ee20f978197cfd3a43b1375c1",
+    url: "http://api.calil.jp/library",
     data: {
+      appkey: "e669768ee20f978197cfd3a43b1375c1",
       geocode: coords.longitude+","+coords.latitude,
       limit: 5,
+      format: "json"
+    }
+  })
+}
+
+export function accessLibrary (isbn, systemid) {
+  console.log(isbn)
+  console.log(systemid)
+  return $.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    url: "http://api.calil.jp/check",
+    data: {
+      appkey: "e669768ee20f978197cfd3a43b1375c1",
+      isbn: isbn,
+      systemid: systemid,
       format: "json"
     }
   })
--- js/main.js
+++ js/main.js
@@ -7,8 +7,19 @@ inputInsb[1].onclick = onClickToSearch;
 
 if (!navigator.geolocation) alert("この端末では位置情報が取得できません。");
 
-function onClickToSearch() {
-  calilApi.getLibrary(coords).then((res) => console.log(res))
+function onClickToSearch () {
+  calilApi.getLibrary(coords)
+    .then(checkBookExist)
+    .then((res) => console.log(res))
+}
+
+// 
+function checkBookExist (librarys) {
+  let systemids = ""
+  for(let lib of librarys) {
+    systemids += (','+lib.systemid)
+  } 
+  return calilApi.accessLibrary(inputInsb[0].value, systemids.slice(1))
 }
 
 // 現在地を取得
@@ -17,7 +28,7 @@ navigator.geolocation.getCurrentPosition(_successGetLocation, _failedGetLocation
 function _successGetLocation (pos) {
   coords = pos.coords
   inputInsb[0].disabled = inputInsb[1].disabled = false
-  inputInsb[0].placeholder = '本のINSB-10を入力（本の名前とか受け付けないよ）'
+  inputInsb[0].placeholder = '本のINSBを入力（本の名前とか受け付けないよ）'
 }
 
 function _failedGetLocation (err) {
